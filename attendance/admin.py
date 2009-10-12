121b5fc4ca8a53ab4ec7e3b6e641257cb47cb99c
Philipp Bosch <pb@philippbosch.de>
2009-10-12 14:16:26 +0200

    Admin actions pay/unpay added

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from attendance.models import OfficeDay
 
+def pay(modeladmin, request, queryset):
+    queryset.update(paid=True)
+pay.short_description = "Mark as payed"
+
+
+def unpay(modeladmin, request, queryset):
+    queryset.update(paid=False)
+unpay.short_description = "Mark as unpayed"
+
+
 class OfficeDayAdmin(admin.ModelAdmin):
     list_display = ('user','day','paid',)
     list_display_links = ('user', 'day',)
     list_filter = ('user','paid',)
+    actions = [pay,unpay]
 
 admin.site.register(OfficeDay, OfficeDayAdmin)
